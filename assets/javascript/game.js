b3790c0fe5869a70d49d4710dd0aef24776141b2
chinjon <Jonathankchin@gmail.com>
2016-12-01 12:35:20 -0500

    working out basis for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,37 @@
 var characterOne = {
-  "name" : "character one",
-  "class" : "fighter",
-  "base health" : 200,
-  "base attack" : 30,
-  "base defense" : 9,
-  "base speed" : 4
+    "name": "character one",
+    "class": "fighter",
+    "character selected": false,
+    "stats": {
+        "base health": 200,
+        "base attack": 30,
+        "base defense": 9,
+        "base speed": 4
+    },
+    "levelUp": function() {
+        this.stats["base health"] += 10;
+        this.stats["base attack"] += 10;
+        this.stats["base defense"] += 3;
+        this.stats["base speed"] += 2;
+    }
 }
 
 var characterTwo = {
-  "name" : "character one",
-  "class" : "fighter",
-  "character selected" : false,
-  "stats" : {
-    "base health" : 300,
-    "base attack" : 5,
-    "base defense" : 26,
-    "base speed" : 11
-  },
-
-  "levelUp": function() {
-    this.stats["base health"] += 25;
-    this.stats["base attack"] += 3;
-    this.stats["base defense"] += 6;
-    this.stats["base speed"] += 4;
-  }
+    "name": "character one",
+    "class": "fighter",
+    "character selected": false,
+    "stats": {
+        "base health": 300,
+        "base attack": 5,
+        "base defense": 26,
+        "base speed": 11
+    },
+    "levelUp": function() {
+        this.stats["base health"] += 25;
+        this.stats["base attack"] += 3;
+        this.stats["base defense"] += 6;
+        this.stats["base speed"] += 4;
+    }
 
 }
 
@@ -31,14 +39,15 @@ var characterTwo = {
 // need to figure out how to live update stats
 
 $(document).ready(function() {
-  var availStats = ["base health", "base attack", "base defense" , "base speed"];
-  var size = Object.keys(characterTwo["stats"]).length;
-  function displayStats() {
-    for(var i = 0; i < size; i++) {
-      var characterStat = $("<div>").addClass("characterStats").data("stat-point", characterTwo["stats"][availStats[i]]).html(availStats[i] + ": " + characterTwo.stats[availStats[i]]).appendTo("#characterStats");
+    var availStats = ["base health", "base attack", "base defense", "base speed"];
+    var size = Object.keys(characterTwo["stats"]).length;
+
+    function displayStats() {
+        for (var i = 0; i < size; i++) {
+            var characterStat = $("<div>").addClass("characterStats").data("stat-point", characterTwo["stats"][availStats[i]]).html(availStats[i] + ": " + characterTwo.stats[availStats[i]]).appendTo("#characterStats");
+        }
     }
-  }
-  displayStats();
+    displayStats();
 
 })
 
@@ -46,13 +55,13 @@ $(document).ready(function() {
 
 
 function basicAttack(defender, attacker) {
-  if(defender["base defense"] < attacker["base attack"]) {
-    defender["base health"] -= attacker["base attack"];
-    console.log("Attack was successful");
-  } else {
-    defender["base health"] -= 1;
-    console.log("Attack was not very effective.");
-  }
+    if (defender["base defense"] < attacker["base attack"]) {
+        defender["base health"] -= attacker["base attack"];
+        console.log("Attack was successful");
+    } else {
+        defender["base health"] -= 1;
+        console.log("Attack was not very effective.");
+    }
 }
 
 console.log(characterOne["base health"]);
